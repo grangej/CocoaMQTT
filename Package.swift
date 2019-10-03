b39a41a2725a43c95779b92f6f8adff346ce8494
John Grange <john@sd-networks.net>
2019-10-03 10:02:06 -0700

    WIP SPM Support

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "CocoaMQTT",
+    platforms: [
+        .macOS(.v10_14),
+        .iOS(.v10)
+    ],
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "CocoaMQTT",
+            targets: ["CocoaMQTT"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/grangej/CocoaAsyncSocket.git", from: "7.6.0"),
+    ],
+    targets: [
+        .target(
+            name: "CocoaMQTT",
+            dependencies: ["CocoaAsyncSocket"],
+            path: "Source"),
+    ]
+)
--- Source/CocoaMQTT.swift
+++ Source/CocoaMQTT.swift
@@ -9,7 +9,6 @@
 import Foundation
 import CocoaAsyncSocket
 
-
 /**
  * QOS
  */
